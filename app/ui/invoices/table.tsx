d9c730d7e38ba63b74adfa1d3c4d2e0f73789549
Ian <Qazzian@gmail.com>
2024-12-02 23:24:00 +0000

    invoice search

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -4,13 +4,15 @@ import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
+interface TableParams {
+  query: string;
+  currentPage: number;
+}
+
 export default async function InvoicesTable({
   query,
   currentPage,
-}: {
-  query: string;
-  currentPage: number;
-}) {
+}: TableParams) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,18 @@
 'use client';
 
+import React from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {useSearchParams, usePathname, useRouter } from "next/navigation.js";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  console.log(`search params: ${searchParams.toString()}`);
+
+  const debouncedSearch = useDebouncedCallback(handleSearch, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,8 +21,23 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={debouncedSearch}
+        defaultValue={searchParams.get('search')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    const params = new URLSearchParams(searchParams);
+    const term = event.target.value;
+    console.log(`Searching... ${term}`);
+
+    if (term) {
+      params.set('search', term);
+    } else {
+      params.delete('search');
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  }
 }
