cff27a7f572c58d3ed59d8e826a43b5f2aa12233
Ian <Qazzian@gmail.com>
2024-12-13 16:56:02 +0000

    Save invoices to DB

---
+++ app/lib/actions.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import {z} from 'zod';
+import {sql} from '@vercel/postgres';
+
+const FormSchema= z.object({
+	id: z.string(),
+	customerId: z.string(),
+	amount: z.coerce.number(),
+	status: z.enum(['pending', 'paid']),
+	data: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({id: true, data: true});
+
+export async function createInvoice(formData: FormData) {
+	console.log('FORM DATA', formData);
+
+	const  { customerId, amount, status } = CreateInvoice.parse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	});
+	console.log(typeof amount);
+	const amountInCents = amount * 100;
+	const date = new Date().toISOString().split('T')[0];
+
+	await sql`
+		insert into invoices (customer_id, amount, status, date)
+		values (${customerId}, ${amountInCents}, ${status}, ${date})
+	`;
+}
+
